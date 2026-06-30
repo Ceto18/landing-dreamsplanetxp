b54504e455b65fb9c4064b4926f129d400ac8fe8
CetoDev <acervantest19@gmail.com>
2026-06-30 17:27:35 -0500

    feat: add dynamic export and revalidation settings in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 import { Header } from '@/components/header'
 import { Hero } from '@/components/hero'
 import { Mission } from '@/components/pages/mission/mission'
