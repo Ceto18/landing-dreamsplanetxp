e984ce30a9d8ae7a275c654f0ba1a9163189af56
CetoDev <acervantest19@gmail.com>
2026-07-27 10:38:15 -0500

    feat: remove lastModified property from sitemap entries for cleanup

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,3 @@
-// src/app/sitemap.ts
-
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -8,43 +6,36 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: baseUrl,
-            lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1,
         },
         {
             url: `${baseUrl}/mission`,
-            lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.9,
         },
         {
             url: `${baseUrl}/about`,
-            lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
             url: `${baseUrl}/review`,
-            lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.7,
         },
         {
             url: `${baseUrl}/aviso-legal`,
-            lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.3,
         },
         {
             url: `${baseUrl}/condiciones-generales`,
-            lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.3,
         },
         {
             url: `${baseUrl}/politica-de-privacidad`,
-            lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 0.3,
         },
